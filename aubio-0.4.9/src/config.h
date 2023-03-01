9f2466a5f3e36b6e88e2275201d6ef61ae0a0cd9
tantanGH <tantangh68@gmail.com>
2023-03-02 06:47:32 +0900

    git cacge clear

--- aubio-0.4.9/src/config.h
+++
@@ -1,8 +0,0 @@
-#define HAVE_STDLIB_H
-#define HAVE_STDIO_H
-//#define HAVE_COMPLEX_H
-#define HAVE_MATH_H
-#define HAVE_STRING_H
-#define HAVE_ERRNO_H
-#define HAVE_LIMITS_H
-#define HAVE_AUBIO_DOUBLE 1
